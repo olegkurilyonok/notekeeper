04559f05240bd1390d6f7b4e96a9cbf82de49d63
Aleh Kurylionak <oleg.kurilyonok@gmail.com>
2023-06-22 12:05:58 +0200

    initial commit

---
+++ src/main/java/com/example/notekeeper/NoteKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.notekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteKeeperApplication.class, args);
+    }
+
+}
